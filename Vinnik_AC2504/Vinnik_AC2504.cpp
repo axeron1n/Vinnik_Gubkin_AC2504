e1cffd0f3ace0cf3a6a330f23af0915498f9a82f
axeron1n <89777859537vav@gmail.com>
2026-09-02 19:37:49 +0300

    добавление ввода и вывода переменной x

--- Vinnik_AC2504/Vinnik_AC2504.cpp
+++ Vinnik_AC2504/Vinnik_AC2504.cpp
@@ -2,10 +2,15 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
     std::cout << "Hello World!\n";
+    int x;
+    cin >> x;
+    cout << 10 / x;
+    return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
